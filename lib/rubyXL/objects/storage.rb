c05439ebd48730323b7b2375c50d6efc49776f3e
James Roane Draths <jdraths@alumni.nd.edu>
2017-03-02 18:23:52 -0600

    content type

--- lib/rubyXL/objects/storage.rb
+++ lib/rubyXL/objects/storage.rb
@@ -169,7 +169,7 @@ module RubyXL
 
     def attach_relationship(rid, rf)
       case rf
-      when RubyXL::ChartFile       then store_relationship(rf) # TODO
+      when RubyXL::ActiveXBinary       then store_relationship(rf) # TODO
       else store_relationship(rf, :unknown)
       end
     end
